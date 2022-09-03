391bcd1fd0838415763a3713a509a5fb179823a1
Saija <saija.seppa@tuni.fi>
2022-09-03 23:54:35 +0300

    5.1

---
+++ src/components/LoginForm.js
@@ -0,0 +1,30 @@
+
+/*const LoginForm = () => {
+  return (
+
+    <div>
+      <h2>Log in to application</h2>
+      <form onSubmit={logIn}>
+        <div>
+          username:
+          <input
+            type="text"
+            value={userName}
+            onChange={({ target }) => setUserName(target.value)} />
+        </div>
+        <div>
+          password:
+          <input
+            type="password"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)} />
+        </div>
+        <div>
+          <button type="submit">login</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default LoginForm*/
